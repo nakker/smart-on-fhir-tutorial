b30d0cd7e3dc2d284ba8dc44581c63917f107b74
nakker <nakker@gmail.com>
2019-06-24 19:45:28 -0600

    no message

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -107,7 +107,11 @@ function fetchall(smart, name) {
                 if(object) {
                     console.log("-----------------"+obj_name+"------------------");
                     console.log(object);
-                    p.content += "<h2>"+obj_name+"</h2><p style='font-size:10px'>" + object.text.div + '</p>';
+                    
+                    p.content += "<h2>"+obj_name+"</h2>";
+                    object.forEach(function (o){
+                        p.content += "<p style='font-size:10px'>" + o.text.div + '</p>';
+                    }
                     props_loaded += 1;
                     is_resolved(props_loaded, properties, p, ret);
                 }
